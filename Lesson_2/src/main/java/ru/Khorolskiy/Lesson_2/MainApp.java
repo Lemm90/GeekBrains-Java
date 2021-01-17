efef3fa800ef60a278c8d786c761f1ee60ebba4c
Petr <khorolskii@gmail.com>
2021-01-18 00:45:57 +0300

    add mission 4, 5

--- Lesson_2/src/main/java/ru/Khorolskiy/Lesson_2/MainApp.java
+++ Lesson_2/src/main/java/ru/Khorolskiy/Lesson_2/MainApp.java
@@ -5,8 +5,14 @@ public class MainApp {
 
     public static void main(String[] args) {
     changingAnArray();
+        System.out.println();
     fillingArrey();
+        System.out.println();
     increaseInTheNumbers();
+        System.out.println();
+    square();
+        System.out.println();
+    minMax();
     }
 
 //адать целочисленный массив, состоящий из элементов 0 и 1.
@@ -42,20 +48,61 @@ public class MainApp {
         for (int i = 0; i < arr.length; i ++) {
             if (arr[i] < 6){
                 arr[i] = arr[i] * 2;
-
             }
         }
         System.out.println(Arrays.toString(arr));
-                        }
-       // System.out.println(Arrays.toString(arr));
-
-
-
         }
 
+// п.4 Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+// и с помощью цикла(-ов) заполнить его диагональные элементы единицами
+// (можно только одну из диагоналей, если обе сложно).
+    public static void  square () {
+        int [] [] arr2d = new int [5] [5];
+        arr2d [0] [0] = 1;
+        arr2d [1] [1] = 1;
+        arr2d [2] [2] = 1;
+        arr2d [3] [3] = 1;
+        arr2d [4] [4] = 1;
+        arr2d [0] [4] = 1;
+        arr2d [1] [3] = 1;
+        arr2d [3] [1] = 1;
+        arr2d [4] [0] = 1;
+        for (int i = 0; i < arr2d.length; i++) {
+            for (int j = 0; j < arr2d.length; j++) {
+                System.out.print(arr2d [i] [j] + " ");
+            }
+                        System.out.println();
+        }
+
+        }
+// ** Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
+    public static void minMax (){
+        int [] arr = {8,7,15,3,65,23,7,2,67,34};
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i] ) {
+                max = arr[i];
+            } else if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        System.out.println( "Max: " + max);
+        System.out.println("Min: " + min);
+
+    }
+//** Написать метод, в который передается не пустой одномерный целочисленный массив,
+// метод должен вернуть true, если в массиве есть место, в котором сумма левой и правой части массива равны.
+    public static boolean sum (){
+        int [] myArr = new int[];
+
+    }
+    }
 
 
-        //System.out.println(Arrays.toString(arr));
+
+
+
 
 
 
